d765d5d98c74ef219f75fae144301764da94c555
Ralph Lauren <81021854+abuizid@users.noreply.github.com>
2022-03-14 22:16:59 +0300

    запустите сначала сайт

--- src/script.js
+++ src/script.js
@@ -1,17 +1,8 @@
-// создание свойства класса без конструктора
-class Game {
-    name = 'Violin Charades'
-}
-const myGame = new Game()
+import "./style.css";
 
-// создаем параграф
-const p = document.createElement('p')
-p.textContent = `I like ${myGame.game}.`
+const root = document.getElementById("root");
+const heading = document.createElement("h1");
+heading.className = "head";
+heading.textContent = "УВАЖАЕМЫЕ КОЛЛЕГИ, ГОЛОДНЫЕ ИГРЫ ОБЪЯВЛЯЮ ОТКРЫТЫМИ!";
 
-// создаем элемент заголовка
-const heading = document.createElement('h1')
-heading.textContent = 'Как интересно!'
-
-// добавляем параграф и заголовок в DOM
-const root = document.querySelector('#root')
-root.append(heading, p)
+root.append(heading);
