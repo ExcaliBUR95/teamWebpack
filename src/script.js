d49e40ee7a555d25286f121de98e6f8efc4e8a5b
Ralph Lauren <81021854+abuizid@users.noreply.github.com>
2022-03-14 21:38:24 +0300

    its work you know?

--- src/script.js
+++ src/script.js
@@ -1,2 +1,17 @@
-var square = function(number) { return number * number; };
-var x = square(4); // x получает значение 16
+// создание свойства класса без конструктора
+class Game {
+    name = 'Violin Charades'
+}
+const myGame = new Game()
+
+// создаем параграф
+const p = document.createElement('p')
+p.textContent = `I like ${myGame.game}.`
+
+// создаем элемент заголовка
+const heading = document.createElement('h1')
+heading.textContent = 'Как интересно!'
+
+// добавляем параграф и заголовок в DOM
+const root = document.querySelector('#root')
+root.append(heading, p)
